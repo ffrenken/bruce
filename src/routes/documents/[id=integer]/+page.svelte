06646131130d1a5d0fc1c0e430a87381f7b904d9
Florian Frenken <florian.frenken@rwth-aachen.de>
2025-07-29 16:04:17 +0200

    Handle basic segmentation logic

--- src/routes/documents/[id=integer]/+page.svelte
+++ src/routes/documents/[id=integer]/+page.svelte
@@ -1,5 +1,118 @@
 <script lang="ts">
 	const { data } = $props();
+
+	const content = data.document.content;
+
+	// true if span at index marked as boundary
+	let input = $state<boolean[]>([]);
+
+	function handleInput(e: KeyboardEvent) {
+		if (input.length >= content.length) {
+			return;
+		}
+		switch (e.key) {
+			case ' ':
+			case 'Enter':
+				e.preventDefault();
+				input.push(e.key === 'Enter');
+				break;
+		}
+	}
+
+	type Span = { id: string; text: string; metadata: string };
+	const segments = $derived.by(() => {
+		return input.reduce(
+			(result, isBoundary, i) => {
+				if (isBoundary) {
+					result.push([content[i]]);
+				} else {
+					result[result.length - 1].push(content[i]);
+				}
+				return result;
+			},
+			[[]] as Span[][]
+		);
+	});
 </script>
 
-{data.document.experiment}:{data.document.name}
+<svelte:window onkeydown={handleInput} />
+
+<div>
+	{#each segments as segment, i}
+		<p>
+			{#each segment as span (span.id)}
+				<span>{span.text}</span>
+			{/each}
+			<!-- current span after last segment if not finished -->
+			{#if i === segments.length - 1 && input.length < content.length}
+				<span class="caret">{content[input.length].text}</span>
+			{/if}
+		</p>
+	{/each}
+</div>
+
+<style>
+	div {
+		width: 50%;
+		box-shadow:
+			0 1px 3px 0 #9e829c,
+			0 1px 2px -1px #9e829c;
+		background-color: #f9f9f9;
+	}
+
+	p {
+		padding-block: 0.25em;
+		padding-inline: 0.75em;
+		text-align: justify;
+		transition: background-color 0.2s ease-in-out;
+
+		&:hover {
+			background-color: #f0eff4;
+		}
+	}
+
+	/*
+	no divider after the last span in
+	each segment and before the caret
+	*/
+	p span:not(:last-child):not(:has(+ .caret)) {
+		border-inline-start-width: 0px;
+		border-inline-end-width: 0.1em;
+		border-block: 0;
+		border-color: #9e829c;
+		border-style: dashed;
+	}
+
+	p span:not(:first-child) {
+		padding-left: 0.5ch;
+	}
+
+	p span:not(:last-child) {
+		padding-right: 0.5ch;
+	}
+
+	.caret {
+		position: relative;
+		font-weight: bold;
+
+		&::before {
+			position: absolute;
+			content: '|';
+			font-weight: 100;
+			top: -0.25em;
+			left: -0.25em;
+			animation: blink 1s step-end infinite;
+		}
+	}
+
+	@keyframes blink {
+		from,
+		to {
+			color: transparent;
+		}
+
+		50% {
+			color: black;
+		}
+	}
+</style>
